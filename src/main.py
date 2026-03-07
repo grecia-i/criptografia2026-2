6fecf3de68265f783fb1de133e505bcac22d7176
lued2897 <lued2897@gmail.com>
2026-03-06 20:28:16 -0600

    cli for commands

--- src/main.py
+++ src/main.py
@@ -2,39 +2,52 @@ import os
 import argparse
 from encryption.encrypt import *
 from decryption.decrypt import *
+from parser.parser import build_parser
 
 VAULT_PATH = "vault_container"
 KEY_PATH = os.path.join(VAULT_PATH, "key", "priv.key")
 
 def main():
-    parser = argparse.ArgumentParser(description="A secure file encryption module")
-    parser.add_argument("--f",nargs='?', type=str, help="The path of the file to be encrypted")
+    # parser = argparse.ArgumentParser(description="A secure file encryption module")
+    # parser.add_argument("file",nargs='?', type=str, help="The path of the file to be encrypted")
+    # parser.add_argument("command",nargs="", type=str,choices=["encrypt","decrypt"])
+    # args = parser.parse_args()
+    parser = build_parser()
     args = parser.parse_args()
-    #print(args.f)
-    if args.f == None:
-        file_name = input("Enter the name of the file (e.g., secret.txt):  ")
-    else:
-        file_name = args.f
-
-
-    if not (os.path.exists(file_name)):
-        print(f"ERROR, {file_name} does not exist")
-        return
-
-    if os.path.exists(KEY_PATH):
+    print(args)
+    
+    if os.path.isfile(KEY_PATH):
         key=read_key(KEY_PATH)
-        print("Key read from file, key is: \n", key.hex())
+        print("Key read from file, key is: \n", key.hex()) #debug
     else:
         key = generate_key()
         write_key(KEY_PATH,key)
-        print("New key generated, key is: \n", key.hex())
+        print("New key generated, key is: \n", key.hex()) #debug
 
-    vault_container = os.path.join(VAULT_PATH, file_name)
-    print("Vault container = ", vault_container)
 
-    encrypt_file(file_name, vault_container, key)
+    if(args.command == "encrypt"):
+        file_name = args.input_file
+        if not (os.path.isfile(file_name)):
+            print(f"ERROR, {file_name} does not exist")
+            return
+
+        vault_container = os.path.join(VAULT_PATH, file_name)
+        print("Vault container = ", vault_container) #debug
+        encrypt_file(file_name, vault_container, key)
+
+
+    elif(args.command =="decrypt"):
+        vault_container = args.container_dir
+        if os.path.isdir(vault_container):
+            decrypt_container(vault_container, args.output_file, key)
+
+
+        
+
+
     
-    decrypt_container(vault_container, "recovered.txt", key)
+    
+    
 
 if __name__ == "__main__":
     main()
---
+++ src/parser/parser.py
@@ -0,0 +1,44 @@
+import argparse
+
+def build_parser():
+    parser = argparse.ArgumentParser(
+        description="Secure vault file encryption tool"
+    )
+
+    subparsers = parser.add_subparsers(dest="command", required=True)
+
+
+    encrypt_parser = subparsers.add_parser(
+        "encrypt",
+        help="Encrypt a file into a vault container"
+    )
+
+    encrypt_parser.add_argument(
+        "input_file",
+        type=str,
+        help="File to encrypt"
+    )
+
+
+    decrypt_parser = subparsers.add_parser(
+        "decrypt",
+        help="Decrypt a vault container"
+    )
+
+    decrypt_parser.add_argument(
+        "container_dir",
+        type=str,
+        help="Vault container directory"
+    )
+
+    decrypt_parser.add_argument(
+        "output_file",
+        type=str,
+        help="Recovered plaintext file"
+    )
+    args = parser.parse_args()
+
+    return parser
+
+
+#args = parser.parse_args()
